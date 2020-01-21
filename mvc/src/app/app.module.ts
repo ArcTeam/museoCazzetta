39df24b20eec31c5482035eef847960b378cecc8
Giuseppe Naponiello <beppenapo@arc-team.com>
2020-01-21 14:02:22 +0100

    aggiunta e configurazione del modulo ngx-owl-carousel-o, creazione della galleria immagini nella home

--- mvc/src/app/app.module.ts
+++ mvc/src/app/app.module.ts
@@ -7,28 +7,33 @@ import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RouterModule } from '@angular/router';
 // import { MatToolbarModule,MatButtonModule } from '@angular/material/';
 import { AngularMaterialModule } from './core/core.module';
 
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // MatToolbarModule,MatButtonModule,
     AngularMaterialModule,
     HttpClientModule,
+    CarouselModule,
+    RouterModule.forRoot([]),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
---
+++ mvc/src/app/carousel/carousel.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+  public slides = [
+    { src: "./assets/img/carousel/001.jpg" },
+    { src: "./assets/img/carousel/002.jpg" },
+    { src: "./assets/img/carousel/003.jpg" },
+    { src: "./assets/img/carousel/004.jpg" },
+    { src: "./assets/img/carousel/005.jpg" },
+    { src: "./assets/img/carousel/006.jpg" },
+    { src: "./assets/img/carousel/007.jpg" },
+    { src: "./assets/img/carousel/008.jpg" },
+    { src: "./assets/img/carousel/009.jpg" }
+  ];
+  customOptions: any = {
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    animateOut: 'fadeOut',
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    // navSpeed: 700,
+    // navText: ['', ''],
+    // responsive: {
+    //   0:   { items: 1 },
+    //   400: { items: 1 },
+    //   740: { items: 1 },
+    //   940: { items: 1 }
+    // },
+    nav: false
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
